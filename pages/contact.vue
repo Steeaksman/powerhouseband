dadf3fe2626e2caeee41b1d67a8b1b9a5754dce5
Donny <nstokes142@gmail.com>
2025-05-30 21:53:37 -0700

    added contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,44 @@
 <template>
-    <div>
-This is the contact Page
-    </div>
+    <div class="flex items-center justify-center h-screen h-14 bg-gradient-to-r from-purple-500 to-pink-500">
+      <!-- Card -->
+      <div class="bg-zinc-800 p-2 mx-6 rounded-2xl">
+        <!-- Flex Container -->
+        <div class="flex flex-col md:flex-row rounded-l-xl">
+          <!-- Image -->
+          <img
+            src="/images/Logo_1.png"
+            alt=""
+            class="object-fit rounded-xl h-80 md:h-64 md:rounded-l-xl md:rounded-r-none transform hover:scale-105 hover:rounded-xl duration-200"
+          />
+          <!-- Content -->
+          <div class="p-6 md:p-12">
+            <h2
+              class="font-serif text-2xl font-medium text-center text-white md:text-left"
+            >Contact us for Booking . . .
+            </h2>
+
+            <p
+              class="max-w-xs my-4 text-2xl leading-5 tracking-wide text-center text-white md:text-left"
+            >
+              Ben Stokes
+            </p>
+            <p
+              class="max-w-xs my-4 text-2xl leading-5 tracking-wide text-center text-white md:text-left"
+            >
+              bestbassist1324@gmail.com
+            </p>
+            <p
+              class="max-w-xs my-4 text-2xl leading-5 tracking-wide text-center text-white md:text-left"
+            >
+              (505) 340-7445
+            </p>
+
+           
+            </div>
+          </div>
+        </div>
+      </div>
+   
 </template>
 
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
+     <div class="pb-2 min-h-screen bg-slate-50 ">
     <div>
-Opening Page!!
+        <img src="/images/Red Concert background w Logo_740.jpg" alt="bandlogo"/>
+    </div>
+
+    <div class="container mx-auto px-6 lg:px-0 py-12 text-center">
+
+ <h2 class="md:text-4xl text-2xl font-bold text-gray-800 mb-4">Albuquerques hottest R and B live band!<br></h2>
+
+ <h2 class="md:text-3xl text-2xl font-bold text-gray-800">
+A Powerhouse of music featuring sounds from the 70's to Today
+ </h2>
+
+        
+    </div>
+
     </div>
 </template>
 
