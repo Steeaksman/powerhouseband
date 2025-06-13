369178b4e3e14b914a09c54cbb3c5d4b2da9ebb3
Donny <nstokes142@gmail.com>
2025-06-12 21:48:24 -0700

    shows

--- pages/shows.vue
+++ pages/shows.vue
@@ -5,23 +5,24 @@
 
      <div class="z-10 container mx-auto px-6 lg:px-0 py-12 text-center">
 
-    <h2 class="md:text-3xl text-2xl font-bold text-white">
-    We are appearing at the Santa Ana Casino!! This Friday and Saturday night!!
+    <h2 class="md:text-3xl text-2xl font-bold text-white mb-4">
+    We are appearing at the Santa Ana Casino!! July 4th and 5th!!
     </h2>
-    <div>
-We are appearing at the Santa Ana Casino!! This Friday and Saturday night!!
-    </div>
+    
 
     <div class="md:flex">
-    <div><img src="/images/june6.jpg" alt="bandlogo"/>
+     <div class="">
+        <img src="https://benweb73.s3.us-west-2.amazonaws.com/Red+Concert+background+w+Logo2.jpg" alt="bandlogo"/>
+        <!-- <img src="/images/Red Concert background w Logo_740.jpg" alt="bandlogo"/> -->
     </div>
 
-    <div><h2 class="md:text-3xl text-2xl font-bold text-white">
+    <div><h2 class="md:text-3xl text-2xl font-bold text-white mt-4">
+        
    We will be performing for 2 days
 At the<br>
  SANTA ANA CASINO<br>
-June 6 at 9pm  
-June 7 at 9pm<br>
+July 4 at 9pm<br>
+July 5 at 9pm<br>
 Santa Ana Casino<br>
 6109 Aztec Rd NE,<br>
  Albuquerque, NM 87110
