0e9312bcf2188d2a4e691da32e1c865fe92e3567
Donny <nstokes142@gmail.com>
2026-01-14 15:44:35 -0800

    new  show image

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
 
 
  <footer class="max-h-10 fixed bottom-0 left-0 z-20 w-full p-2 bg-black border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-2 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-base text-gray-50 sm:text-center dark:text-gray-400">© 2025 Powerhouse Showband <br> All rights reserved.
+    <span class="text-base text-gray-50 sm:text-center dark:text-gray-400">© 2026 Powerhouse Showband <br> All rights reserved.
     </span>
     
 </footer> 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+   devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
 })
