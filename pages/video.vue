a450201f79d38ed34ba14a1fa8db56852bc23977
Donny <nstokes142@gmail.com>
2026-03-15 00:04:59 -0700

    update video section layout and add performance heading

--- pages/video.vue
+++ pages/video.vue
@@ -6,12 +6,14 @@
      <div class="container mx-auto px-6 lg:px-0 py-12 text-center">
 
     <h2 class="md:text-3xl text-2xl font-bold text-white mb-4">
-    
+    Check out our Live performances! 
     </h2>
 
 
-<div class="mx-auto mt-32 ">
-    <div style="position:relative;padding-top:56.25%;"><iframe src="https://iframe.mediadelivery.net/embed/453789/0766203d-c973-48da-8275-d5c899b0c851?autoplay=false&loop=false&muted=false&preload=true&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:75%;width:75%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe></div>
+<div class="flex justify-center items-center mt-10">
+    <div class="relative w-3/4" style="padding-top: 56.25%;">
+        <iframe src="https://iframe.mediadelivery.net/embed/453789/f83aed42-e5f1-4ca5-8dcb-02f7a2564409?autoplay=true&loop=false&muted=false&preload=true&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:100%;width:100%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe>
+    </div>
   
 </div>
 
@@ -27,7 +29,11 @@
 
 
 
-<div style="position:relative;padding-top:56.25%;"><iframe src="https://iframe.mediadelivery.net/embed/453789/6fcd71d3-a74c-47af-b96f-dfdcab6b729b?autoplay=false&loop=false&muted=false&preload=true&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:75%;width:75%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe></div>
+<div class="flex justify-center items-center mt-10">
+   <div class="relative w-3/4" style="padding-top: 56.25%;">
+    <iframe src="https://iframe.mediadelivery.net/embed/453789/6fcd71d3-a74c-47af-b96f-dfdcab6b729b?autoplay=false&loop=false&muted=false&preload=true&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:100%;width:100%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe>
+</div>
+</div>
 
 <div class="z-10 container mx-auto px-6 lg:px-0 py-12 text-center">
 
